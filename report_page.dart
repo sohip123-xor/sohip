a6a4820ca4faf3ba79a5c85e7b2c7d80bf4f4aa3
sohip <sohipwaleed73@gmil.com>
2025-09-26 02:21:42 +0300

    صلاح خطأ تسجيل الدخول عند ترك كلمة المرور فارغة

--- report_page.dart
+++ report_page.dart
@@ -5,3 +5,7 @@ import 'entities.dart';
 
 class ReportPage extends StatelessWidget {
   const ReportPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final ScanResult r = ModalRoute.of(context)!.settings.arguments as ScanResult;
+    Color badge(int s)=> s>=80?Colors.green : s>=50?Colors.amber : Colors.red;
