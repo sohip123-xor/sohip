a133ebba3b31d8ea7ab6042c8c369e14a0ee8b2c
sohip <sohipwaleed73@gmil.com>
2025-09-26 02:58:53 +0300

    تنظيف المشروع من الاستدعاءات غير المستخدمة

--- scan_page.dart
+++ scan_page.dart
@@ -24,4 +24,8 @@ class _ScanPageState extends State<ScanPage> {
           IconButton(onPressed: ()=> Navigator.pushNamed(context, '/history'), icon: const Icon(Icons.history))
         ],
       ),
-    
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+           
