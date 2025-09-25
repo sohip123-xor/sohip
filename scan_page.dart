ddd44ebab5c45aa63e98d82ed6e92c861591435e
sohip <sohipwaleed73@gmil.com>
2025-09-26 02:38:07 +0300

    إصلاح مشاكل عرض الواجهة على iPhone SE

--- scan_page.dart
+++ scan_page.dart
@@ -2,3 +2,9 @@ import 'package:flutter/material.dart';
 import 'scan_usecase.dart';
 import 'entities.dart';
 import '/hive_repo.dart';
+
+class ScanPage extends StatefulWidget {
+  const ScanPage({super.key});
+  @override State<ScanPage> createState()=> _ScanPageState();
+}
+
