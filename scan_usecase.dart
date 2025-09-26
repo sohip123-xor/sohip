65c9f81bf869862182266f557c6f25f6ee962661
sohip <sohipwaleed73@gmil.com>
2025-09-26 03:12:59 +0300

    إضافة شرح استخدام في ملف README

---
+++ scan_usecase.dart
@@ -0,0 +1,12 @@
+import 'package:uuid/uuid.dart';
+import 'entities.dart';
+import 'http_header_checker.dart';
+import 'tls_checker.dart';
+import 'port_api_client.dart';
+import 'score_calculator.dart';
+
+class ScanUseCase {
+  final _headers = HttpHeaderChecker();
+  final _tls = TlsChecker();
+  final _ports = PortApiClient();
+  final _score = ScoreCalculator();
