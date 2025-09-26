46eb42be8086603130bad5ff8b5db2e8631f73c3
sohip <sohipwaleed73@gmil.com>
2025-09-26 17:01:54 +0300

    تهيئة قواعد ESLint للفحص التلقائي

--- tls_checker.dart
+++ tls_checker.dart
@@ -4,3 +4,6 @@ import 'entities.dart';
 class TlsChecker {
   Future<TlsFindings> check(Uri uri) async {
     final isHttps = uri.scheme == 'https';
+    if (!isHttps) return TlsFindings(isHttps: false, handshakeOk: false, tlsVersion: null, certValidLikely: false);
+    try {
+     
