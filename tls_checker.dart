84f784cc08506b7e294b3bbc52b7cd12f25c667b
sohip <sohipwaleed73@gmil.com>
2025-09-26 17:06:30 +0300

    ضبط إعدادات خاصة بالبيئة (development/production)

--- tls_checker.dart
+++ tls_checker.dart
@@ -18,3 +18,10 @@ class TlsChecker {
     }
   }
 }
+
+class TlsChecker {
+  Future<TlsFindings> check(Uri uri) async {
+    final isHttps = uri.scheme == 'https';
+    if (!isHttps) return TlsFindings(isHttps: false, handshakeOk: false, tlsVersion: null, certValidLikely: false);
+    try {
+  
